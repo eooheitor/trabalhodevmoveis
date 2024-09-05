7200d6daf039d26a1c0fc4be1c98aad57ee6817b
Heitor <heitor.gb@outlook.com>
2024-09-05 19:34:12 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,160 @@
+<header>
+    <?php include 'menu.php' ?>
+</header>
+
+<div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="owl-carousel header-carousel position-relative">
+        <div class="owl-carousel-item position-relative" data-dot="<img src='img/banners/bannerflamengo.png'>">
+            <img class="img-fluid" src="img/banners/bannerflamengo.png" alt="">
+            <div class="owl-carousel-inner">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-xxl py-5" id="produtos">
+    <div class="container">
+        <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <h6 class="text-primary">Produtos</h6>
+            <h1 class="mb-4">Confira alguns de nossos produtos</h1>
+        </div>
+        <div class="row mt-n2 wow fadeInUp" data-wow-delay="0.3s">
+            <div class="col-12 text-center">
+                <ul class="list-inline mb-5" id="portfolio-flters">
+                    <li class="mx-2 active" data-filter="*">Todos</li>
+                    <li class="mx-2" data-filter=".first">Seleções</li>
+                    <li class="mx-2" data-filter=".second">Premier League</li>
+                    <li class="mx-2" data-filter=".third">Série A</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row g-4 portfolio-container wow fadeInUp" data-wow-delay="0.5s">
+            <div class="col-lg-4 col-md-6 portfolio-item first">
+                <div class="portfolio-img rounded overflow-hidden">
+                    <img class="img-fluid" src="img/camisas/Selecoes/frente-brasil.jpg" alt="">
+                    <div class="portfolio-btn">
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href="img/camisas/Selecoes/frente-brasil.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=""><i class="fa fa-link"></i></a>
+                    </div>
+                </div>
+                <div class="pt-3">
+                    <p class="text-primary mb-0">Seleções</p>
+                    <hr class="text-primary w-25 my-2">
+                    <h5 class="lh-base">Brasil Home 2022</h5>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item second">
+                <div class="portfolio-img rounded overflow-hidden">
+                    <img class="img-fluid" src="img/camisas/Selecoes/costa-brasil.jpg" alt="">
+                    <div class="portfolio-btn">
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href="img/camisas/Selecoes/costa-brasil.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=""><i class="fa fa-link"></i></a>
+                    </div>
+                </div>
+                <div class="pt-3">
+                    <p class="text-primary mb-0">Lorem ipsum</p>
+                    <hr class="text-primary w-25 my-2">
+                    <h5 class="lh-base">Lorem</h5>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item third">
+                <div class="portfolio-img rounded overflow-hidden">
+                    <img class="img-fluid" src="img/camisas/selecoes/detalhe-brasil.jpg" alt="">
+                    <div class="portfolio-btn">
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href="img/camisas/selecoes/detalhe-brasil.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=""><i class="fa fa-link"></i></a>
+                    </div>
+                </div>
+                <div class="pt-3">
+                    <p class="text-primary mb-0">Lorem Ipsum</p>
+                    <hr class="text-primary w-25 my-2">
+                    <h5 class="lh-base">Lorem</h5>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item first">
+                <div class="portfolio-img rounded overflow-hidden">
+                    <img class="img-fluid" src="img/camisas/selecoes/detalhe-brasil.jpg" alt="">
+                    <div class="portfolio-btn">
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href="img/camisas/selecoes/detalhe-brasil.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=""><i class="fa fa-link"></i></a>
+                    </div>
+                </div>
+                <div class="pt-3">
+                    <p class="text-primary mb-0">Lorem</p>
+                    <hr class="text-primary w-25 my-2">
+                    <h5 class="lh-base">Lorem Ipsum</h5>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item second">
+                <div class="portfolio-img rounded overflow-hidden">
+                    <img class="img-fluid" src="img/camisas/selecoes/detalhe-brasil.jpg" alt="">
+                    <div class="portfolio-btn">
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href="img/camisas/selecoes/detalhe-brasil.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=""><i class="fa fa-link"></i></a>
+                    </div>
+                </div>
+                <div class="pt-3">
+                    <p class="text-primary mb-0">Lorem</p>
+                    <hr class="text-primary w-25 my-2">
+                    <h5 class="lh-base">Lorem Ipsum</h5>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item third">
+                <div class="portfolio-img rounded overflow-hidden">
+                    <img class="img-fluid" src="img/camisas/selecoes/detalhe-brasil.jpg" alt="">
+                    <div class="portfolio-btn">
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href="img/camisas/selecoes/detalhe-brasil.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-light rounded-circle mx-1" href=""><i class="fa fa-link"></i></a>
+                    </div>
+                </div>
+                <div class="pt-3">
+                    <p class="text-primary mb-0">Lorem</p>
+                    <hr class="text-primary w-25 my-2">
+                    <h5 class="lh-base">Lorem Ipsum</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid bg-dark text-body footer mt-12 pt- wow fadeIn" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="row g-12">
+            <div class="col-lg-4 col-md-6">
+                <h5 class="text-white mb-4">Contato</h5>
+                <p class="mb-2"><i class="bi bi-whatsapp me-3"></i> (47) 9 92842679</p>
+                <p class="mb-2"><i class="fa fa-envelope me-3"></i>heitor.gb@outlook.com</p>
+                <div class="d-flex pt-2">
+                    <a class="btn btn-square btn-outline-light btn-social" href="https://www.instagram.com/estacao_camisa10/" target="_blank"><i class="fab fa-instagram"></i></a>
+                    <a class="btn btn-square btn-outline-light btn-social" href="https://twitter.com/eooheitor" target="_blank"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-square btn-outline-light btn-social" href="https://www.linkedin.com/in/heitor-berischmeier-b0a225258/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h5 class="text-white mb-4">Links Úteis</h5>
+                <a class="btn btn-link" href="">Home</a>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h5 class="text-white mb-4">Fique atento as novidades</h5>
+                <div class="position-relative mx-auto" style="max-width: 400px;">
+                    <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="number" placeholder="Seu número">
+                    <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">Enviar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="copyright">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a href="#">Estação Camisa10</a>, Todos os direitos reservados
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="js/main.js"></script>
---
+++ menu.php
@@ -0,0 +1,53 @@
+<head>
+    <html lang="pt-br">
+    <meta charset="utf-8">
+    <title>Estação Camisa10 - A melhor loja de camisas</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="img/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="img/icons/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/icons/favicon-16x16.png">
+    <link rel="mask-icon" href="img/icons/safari-pinned-tab.svg" color="#1A2959">
+    <link rel="shortcut icon" href="img/icons/favicon.ico">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
+        <a href="index.php" class="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
+            <img src="img/logos/Ativo.png" alt="logo" class="logo">
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.php" class="nav-item nav-link active">Home</a>
+                <a href="#produtos" class="nav-item nav-link active">Produtos</a>
+            </div>
+        </div>
+    </nav>
+</body>
